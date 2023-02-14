66a619e5f26e8f3491dd2933542324003c918bc1
SeanRowland7 <seanwrowland@gmail.com>
2023-02-14 16:30:19 -0500

    Worked on JSONParser

--- src/input/parser/JSONParser.java
+++ src/input/parser/JSONParser.java
@@ -30,13 +30,36 @@ public class JSONParser
 
 	public ComponentNode parse(String str) throws ParseException
 	{
-		// Parsing is accomplished via the JSONTokenizer class.
+		// Parsing is accomplished via the JSONTokenizer class. 
 		JSONTokener tokenizer = new JSONTokener(str);
 		JSONObject  JSONroot = (JSONObject)tokenizer.nextValue();
 
         // TODO: Build the whole AST, check for return class object, and return the root
+		
+		String description = JSONroot.getString("Description");
+		
+		JSONArray pointsAsJSONArray = JSONroot.getJSONArray("Points");
+		
+		PointNodeDatabase points = new PointNodeDatabase();
+		
+		for(Object point : pointsAsJSONArray)
+		{
+			//points.put(convertToJavaPoint(point));
+		}
+		
+		// Repeat for segment nodes
+		
+		// return new FigureNode(description, points, segments);
+		
 	}
 
-    // TODO: implement supporting functionality
+	
+	// TODO: convert JSON points into actual points
+	private PointNode convertToJavaPoint() 
+	{
+		// get JSON Object "name"
+		// get JSON Object "y"
+		// ....
+	}
 
 }
